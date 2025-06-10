77a1ab82eda4fbba6333d74d1fbdb825135c70e4
srikrishnadeepak718 <deepakbsk.business@gmail.com>
2025-06-10 16:51:25 +0530

    Delete app/civitix directory

--- app/civitix/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function CivitixLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-black text-white px-6 py-12">
-      {children}
-    </div>
-  );
-}
-
